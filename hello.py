d3a71fc4a7b89e224ace9939980db3bad344dade
Song InTae <dlsxo1023@naver.com>
2024-03-13 10:35:15 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+#hello.py example
+print("hello.py")
+print("help, git workshop!")
+print("second commit")
+
+flag = True
+
+if flag:
+    print("Falg is True")
+
+else:
+    print("Flag is False")
